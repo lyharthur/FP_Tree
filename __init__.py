034f759c3484f187551524c27c628c3148f7a5cf
lyharthur <lyharthur@gmail.com>
2016-07-20 11:48:27 +0800

    Create __init__.py

---
+++ __init__.py
@@ -0,0 +1,26 @@
+#coding=utf-8
+import tree_builder
+import tree_miner
+import csv
+with open('20000-out1.csv', newline='') as csvfile:
+    spamreader = csv.reader(csvfile ,delimiter = ',',quotechar = '|')
+    data = []
+    for row in spamreader:
+        row = [num.replace(' ', '') for num in row]
+        data.append(row)
+
+#print(data)
+
+routines = [    
+           ['Cola','Egg','Ham'],
+           ['Cola','Diaper','Beer'],
+           ['Cola','Beer','Diaper','Ham'],
+           ['Diaper','Beer']
+        ]                                  #事务数据集
+
+
+min_sup = 100                             #最小支持度计数
+headerTable = {}        #头结点表，用来存放各个项的索引
+
+treeBuilder = tree_builder.Tree_builder(routines=data, min_sup=min_sup, headerTable=headerTable)    #建造FP_Tree
+tree_miner.Tree_miner(Tree=treeBuilder.tree, min_sup=min_sup, headerTable=headerTable)         #对FP_Tree进行频繁项集的挖掘
